cb22c1c67fa6938449c0d3a5ee8653d247efc136
Franklin Dai <franklindai@Mac.attlocal.net>
2024-12-08 15:39:55 -0600

    first q done

---
+++ 2024/day1/Distances.cpp
@@ -0,0 +1,53 @@
+#include <_stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    ifstream input(argv[1]);
+    vector<int> leftColumn;
+    vector<int> rightColumn;
+    
+    if (input.is_open()) { 
+        std::string line;
+        while (getline(input, line)) { 
+            istringstream iss(line);
+            int left, right;
+
+            if (iss >> left >> right) { 
+                leftColumn.push_back(left);
+                rightColumn.push_back(right);
+            }
+        }
+    }
+
+    sort (leftColumn.begin(), leftColumn.end());
+    sort (rightColumn.begin(), rightColumn.end());
+
+    int sol = 0;
+    for (int i = 0; i < leftColumn.size(); i++) { 
+        sol += abs(leftColumn[i] - rightColumn[i]);
+    }
+    // part 1 sol
+    cout << sol << '\n';
+
+    unordered_map<int, int> rightFreq;
+    for (int i = 0; i < rightColumn.size(); i++) {
+        rightFreq[rightColumn[i]]++;
+    }
+
+    int freq;
+    int sol2 = 0;
+    for (int i = 0; i < leftColumn.size(); i++) { 
+        if (rightFreq.find(leftColumn[i]) != rightFreq.end()) { 
+            sol2 += leftColumn[i] * rightFreq[leftColumn[i]];
+        }  
+    }
+    // part 2 sol
+    cout << sol2 << '\n';
+    return 0;
+}
