d1c400aefebf0777e8b98014adb2fd35585256f0
Flipfloppm <franklin.s.dai@gmail.com>
2024-12-10 22:15:08 -0600

    day2part2 wip

--- 2024/day2/Day2Part2.cpp
+++ 2024/day2/Day2Part2.cpp
@@ -14,44 +14,67 @@ int main(int argc, char *argv[]) {
 
     string line;
     int sol = 0;
+    int idx = 1;
 
     while (getline(input, line)) { 
+        cout << "===" << endl;
         istringstream iss(line);
         vector<int> currLine;
         int num;
         while (iss >> num) { 
             currLine.push_back(num);
         }
-
+        bool removed = false;
         bool valid = true;
         bool increasing;
         if (currLine.size() > 1) { 
-            if ((currLine[1] - currLine[0]) > 0) { 
+            if ((currLine[currLine.size() - 1] - currLine[0]) > 0) { 
                 increasing = true;
             } else {
                 increasing = false;
             }
         } 
 
+        cout << idx << ": ";
         for (int i = 1; i < currLine.size(); i++) {
+            cout << currLine[i] << ' ';
             int diff = currLine[i] - currLine[i - 1];
             if ((diff > 0 && !increasing) || (diff < 0 && increasing)) {
-                valid = false;
-                break;
+                if (removed) {
+                    valid = false;
+                    break;
+                } else {
+                    cout << "removing " << currLine[i] << ' ';
+                    currLine.erase(currLine.begin() + i);
+                    removed = true;
+                    // dec idx because we remove one
+                    i -= 1;
+                }
             } 
 
             if (abs(diff) == 0 || abs(diff) > 3) {
-                valid = false;
-                break;
+                if (removed) {
+                    valid = false;
+                    break;
+                } else {
+                    cout << "removing " << currLine[i] << ' ';
+                    currLine.erase(currLine.begin() + i);
+                    removed = true;
+                    // dec idx because we remove one
+                    i -= 1;
+                }
             }
         }
 
         if (valid) { 
             sol += 1;
+        } else {
+            cout << "INVALID" << endl;
         }
+        idx += 1;
+        cout << endl;
     }
 
-    // part 1 solution
     cout << sol << endl;
     
     return 0;
